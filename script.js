6980924f1ad2f362114f3c5875f1f522fb603068
alberttery16 <147755948+alberttery16@users.noreply.github.com>
2025-05-22 11:35:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,39 @@ gsap.from(".section-text", {
   delay: 0.3,
   ease: "power2.out"
 });
+
+// Hero desvaneciéndose al hacer scroll
+gsap.to("#overlay-text", {
+  scrollTrigger: {
+    trigger: "#hero",
+    start: "top top",
+    end: "bottom top",
+    scrub: true
+  },
+  opacity: 0,
+  y: -100
+});
+
+gsap.to("#bg-video", {
+  scrollTrigger: {
+    trigger: "#hero",
+    start: "top top",
+    end: "bottom top",
+    scrub: true
+  },
+  opacity: 0.2,
+  scale: 1.1
+});
+
+// "Sobre mí" aparece suave desde abajo
+gsap.from("#content", {
+  scrollTrigger: {
+    trigger: "#content",
+    start: "top 80%",
+    toggleActions: "play none none none"
+  },
+  opacity: 0,
+  y: 100,
+  duration: 1.5,
+  ease: "power2.out"
+});
